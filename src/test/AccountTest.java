b704b44a0ff079d83f4d39dc5e48b20c9b75b707
Capa2 <johan931@gmail.com>
2021-02-11 12:13:20 +0100

    setup Setup() in AccountTest

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,12 +1,20 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
 
+    private List<Transaction> transactions;
+    private Customer customer;
+
     @BeforeEach
     void setUp() {
+        this.transactions = new ArrayList<>();
+        this.customer = new Customer("Bot");
     }
 
     @Test
--- src/test/CustomerTest.java
+++ src/test/CustomerTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerTest {
